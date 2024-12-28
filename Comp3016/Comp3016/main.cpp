1bdf87391d9854471806689bf0cad4ba2f0a7b32
Jack Willett <jack-willett@outlook.com>
2024-12-28 15:15:04 +0000

    up to lab 7

---
+++ Comp3016/Comp3016/main.cpp
@@ -0,0 +1,399 @@
+#include <glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include "LoadShaders.h"
+#include "main.h"
+#include "FastNoiseLite.h"
+//GLM
+#include "glm/ext/vector_float3.hpp"
+#include <glm/ext/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+
+using namespace std;
+using namespace glm;
+
+GLuint program;
+
+int windowHeight;
+int windowWidth;
+
+//VAO vertex attribute positions in correspondence to vertex attribute type
+enum VAO_IDs { Triangles, Indices, Colours, Textures, NumVAOs = 2 };
+//VAOs
+GLuint VAOs[NumVAOs];
+
+//Buffer types
+enum Buffer_IDs { ArrayBuffer, NumBuffers = 4 };
+//Buffer objects
+GLuint Buffers[NumBuffers];
+
+//Transformations
+//Relative position within world space
+vec3 cameraPosition = vec3(0.0f, 0.0f, 3.0f);
+//The direction of travel
+vec3 cameraFront = vec3(0.0f, 0.0f, -1.0f);
+//Up position within world space
+vec3 cameraUp = vec3(0.0f, 1.0f, 0.0f);
+
+//Time
+//Time change
+float deltaTime = 0.0f;
+//Last value of time change
+float lastFrame = 0.0f;
+
+//Camera sideways rotation
+float cameraYaw = -90.0f;
+//Camera vertical rotation
+float cameraPitch = 0.0f;
+//Determines if first entry of mouse into window
+bool mouseFirstEntry = true;
+//Positions of camera from given last frame
+float cameraLastXPos = 800.0f / 2.0f;
+float cameraLastYPos = 600.0f / 2.0f;
+
+#define RENDER_DISTANCE 128 //Render width of map
+#define MAP_SIZE RENDER_DISTANCE * RENDER_DISTANCE //Size of map in x & z space
+
+//Amount of chunks across one dimension
+const int squaresRow = RENDER_DISTANCE - 1;
+//Two triangles per square to form a 1x1 chunk
+const int trianglesPerSquare = 2;
+//Amount of triangles on map
+const int trianglesGrid = squaresRow * squaresRow * trianglesPerSquare;
+
+//Generation of height map vertices
+GLfloat terrainVertices[MAP_SIZE][6];
+
+//Positions to start drawing from
+float drawingStartPosition = 1.0f;
+float columnVerticesOffset = drawingStartPosition;
+float rowVerticesOffset = drawingStartPosition;
+
+int rowIndex = 0;
+
+
+
+
+int main()
+{
+    //Initialisation of GLFW
+    glfwInit();
+    //Initialisation of 'GLFWwindow' object
+    windowWidth = 1280;
+    windowHeight = 720;
+    GLFWwindow* window = glfwCreateWindow(windowWidth, windowHeight, "Lab5", NULL, NULL);
+
+    //Checks if window has been successfully instantiated
+    if (window == NULL)
+    {
+        cout << "GLFW Window did not instantiate\n";
+        glfwTerminate();
+        return -1;
+    }
+
+    //Sets cursor to automatically bind to window & hides cursor pointer
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+
+    //Binds OpenGL to window
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    //Load shaders
+    ShaderInfo shaders[] =
+    {
+        { GL_VERTEX_SHADER, "vertexShader.vert" },
+        { GL_FRAGMENT_SHADER, "fragmentShader.frag" },
+        { GL_NONE, NULL }
+    };
+
+    program = LoadShaders(shaders);
+    glUseProgram(program);
+
+
+    glViewport(0, 0, 1280, 720);
+
+    //Sets the framebuffer_size_callback() function as the callback for the window resizing event
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    //Sets the mouse_callback() function as the callback for the mouse movement event
+    glfwSetCursorPosCallback(window, mouse_callback);
+
+    float vertices[] = {
+        //Positions             //Textures
+        0.5f, 0.5f, 0.0f,       1.0f, 1.0f, //top right
+        0.5f, -0.5f, 0.0f,      1.0f, 0.0f, //bottom right
+        -0.5f, -0.5f, 0.0f,     0.0f, 0.0f, //bottom left
+        -0.5f, 0.5f, 0.0f,      0.0f, 1.0f  //top left
+    };
+
+    unsigned int indices[] = {
+        0, 1, 3, //first triangle
+        1, 2, 3 //second triangle
+    };
+
+
+    //Sets index of VAO
+    glGenVertexArrays(NumVAOs, VAOs); //NumVAOs, VAOs
+    //Binds VAO to a buffer
+    glBindVertexArray(VAOs[0]); //VAOs[0]
+    //Sets indexes of all required buffer objects
+    glGenBuffers(NumBuffers, Buffers); //NumBuffers, Buffers
+    //glGenBuffers(1, &EBO);
+
+    //Gets index of colourIn uniform variable
+    GLint colourLocation = glGetUniformLocation(program, "colourIn");
+    //Sets colourIn
+    glUniform4f(colourLocation, 0.5f, -0.5f, 0.0f, 1.0f);
+
+    //Binds vertex object to array buffer
+    glBindBuffer(GL_ARRAY_BUFFER, Buffers[Triangles]);
+    //Allocates buffer memory for the vertices of the 'Triangles' buffer
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    //Binding & allocation for indices
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, Buffers[Indices]);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
+    //Allocation & indexing of vertex attribute memory for vertex shader
+//Positions
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    //Textures
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    //Unbinding
+    glBindVertexArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+    unsigned int texture;
+    //Textures to generate
+    glGenTextures(1, &texture);
+
+    //Binding texture to type 2D texture
+    glBindTexture(GL_TEXTURE_2D, Buffers[Textures]);
+
+    //Selects x axis (S) of texture bound to GL_TEXTURE_2D & sets to repeat beyond normalised coordinates
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    //Selects y axis (T) equivalently
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+    //Parameters that will be sent & set based on retrieved texture
+    int width, height, colourChannels;
+
+    //Model matrix
+    mat4 model = mat4(1.0f);
+    //Scaling to zoom in
+    model = scale(model, vec3(2.0f, 2.0f, 2.0f));
+    //Looking straight forward
+    model = rotate(model, radians(0.0f), vec3(1.0f, 0.0f, 0.0f));
+    //Elevation to look upon terrain
+    model = translate(model, vec3(0.0f, -2.f, -1.5f));
+
+    //Projection matrix
+    mat4 projection = perspective(radians(45.0f), (float)windowWidth / (float)windowHeight, 0.1f, 100.0f);
+
+    for (int i = 0; i < MAP_SIZE; i++)
+    {
+        //Generation of x & z vertices for horizontal plane
+        terrainVertices[i][0] = columnVerticesOffset;
+        terrainVertices[i][1] = 0.0f;
+        terrainVertices[i][2] = rowVerticesOffset;
+
+        //Colour
+        terrainVertices[i][3] = 0.0f;
+        terrainVertices[i][4] = 0.75f;
+        terrainVertices[i][5] = 0.25f;
+
+        //Shifts x position across for next triangle along grid
+        columnVerticesOffset = columnVerticesOffset + -0.0625f;
+
+        //Indexing of each chunk within row
+        rowIndex++;
+        //True when all triangles of the current row have been generated
+        if (rowIndex == RENDER_DISTANCE)
+        {
+            //Resets for next row of triangles
+            rowIndex = 0;
+            //Resets x position for next row of triangles
+            columnVerticesOffset = drawingStartPosition;
+            //Shifts y position
+            rowVerticesOffset = rowVerticesOffset + -0.0625f;
+        }
+    }
+
+    //Generation of height map indices
+    GLuint terrainIndices[trianglesGrid][3];
+
+    //Positions to start mapping indices from
+    int columnIndicesOffset = 0;
+    int rowIndicesOffset = 0;
+
+    //Generation of map indices in the form of chunks (1x1 right angle triangle squares)
+    rowIndex = 0;
+    for (int i = 0; i < trianglesGrid - 1; i += 2)
+    {
+        terrainIndices[i][0] = columnIndicesOffset + rowIndicesOffset; //top left
+        terrainIndices[i][2] = RENDER_DISTANCE + columnIndicesOffset + rowIndicesOffset; //bottom left
+        terrainIndices[i][1] = 1 + columnIndicesOffset + rowIndicesOffset; //top right
+
+        terrainIndices[i + 1][0] = 1 + columnIndicesOffset + rowIndicesOffset; //top right
+        terrainIndices[i + 1][2] = RENDER_DISTANCE + columnIndicesOffset + rowIndicesOffset; //bottom left
+        terrainIndices[i + 1][1] = 1 + RENDER_DISTANCE + columnIndicesOffset + rowIndicesOffset; //bottom right
+
+        //Shifts x position across for next chunk along grid
+        columnIndicesOffset = columnIndicesOffset + 1;
+
+        //Indexing of each chunk within row
+        rowIndex++;
+
+        //True when all chunks of the current row have been generated
+        if (rowIndex == squaresRow)
+        {
+            //Resets for next row of chunks
+            rowIndex = 0;
+            //Resets x position for next row of chunks
+            columnIndicesOffset = 0;
+            //Shifts y position
+            rowIndicesOffset = rowIndicesOffset + RENDER_DISTANCE;
+        }
+    }
+
+    //Allocation & indexing of vertex attribute memory for vertex shader
+    //Positions
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    //Colours
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    //Render loop
+    while (glfwWindowShouldClose(window) == false)
+    {
+        //Time
+        float currentFrame = static_cast<float>(glfwGetTime());
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        //Input
+        ProcessUserInput(window); //Takes user input
+
+        //Rendering
+        glClearColor(0.25f, 0.0f, 1.0f, 1.0f); //Colour to display on cleared window
+        glClear(GL_COLOR_BUFFER_BIT); //Clears the colour buffer
+
+        //Transformations
+        mat4 view = lookAt(cameraPosition, cameraPosition + cameraFront, cameraUp); //Sets the position of the viewer, the movement direction in relation to it & the world up direction
+        mat4 mvp = projection * view * model;
+        int mvpLoc = glGetUniformLocation(program, "mvpIn");
+
+        glUniformMatrix4fv(mvpLoc, 1, GL_FALSE, value_ptr(mvp));
+
+        //Drawing
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glBindVertexArray(VAOs[0]); //Bind buffer object to render; VAOs[0]
+        glDrawElements(GL_TRIANGLES, MAP_SIZE * 32, GL_UNSIGNED_INT, 0);
+
+
+        //Refreshing
+        glfwSwapBuffers(window); //Swaps the colour buffer
+        glfwPollEvents(); //Queries all GLFW events
+    }
+
+
+
+    //Safely terminates GLFW
+    glfwTerminate();
+
+    return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    //Resizes window based on contemporary width & height values
+    glViewport(0, 0, width, height);
+}
+
+void ProcessUserInput(GLFWwindow* WindowIn)
+{
+    //Closes window on 'exit' key press
+    if (glfwGetKey(WindowIn, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(WindowIn, true);
+    }
+
+    //Extent to which to move in one instance
+    const float movementSpeed = 1.0f * deltaTime;
+    //WASD controls
+    if (glfwGetKey(WindowIn, GLFW_KEY_W) == GLFW_PRESS)
+    {
+        cameraPosition += movementSpeed * cameraFront;
+    }
+    if (glfwGetKey(WindowIn, GLFW_KEY_S) == GLFW_PRESS)
+    {
+        cameraPosition -= movementSpeed * cameraFront;
+    }
+    if (glfwGetKey(WindowIn, GLFW_KEY_A) == GLFW_PRESS)
+    {
+        cameraPosition -= normalize(cross(cameraFront, cameraUp)) * movementSpeed;
+    }
+    if (glfwGetKey(WindowIn, GLFW_KEY_D) == GLFW_PRESS)
+    {
+        cameraPosition += normalize(cross(cameraFront, cameraUp)) * movementSpeed;
+    }
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+    //Initially no last positions, so sets last positions to current positions
+    if (mouseFirstEntry)
+    {
+        cameraLastXPos = (float)xpos;
+        cameraLastYPos = (float)ypos;
+        mouseFirstEntry = false;
+    }
+
+    //Sets values for change in position since last frame to current frame
+    float xOffset = (float)xpos - cameraLastXPos;
+    float yOffset = cameraLastYPos - (float)ypos;
+
+    //Sets last positions to current positions for next frame
+    cameraLastXPos = (float)xpos;
+    cameraLastYPos = (float)ypos;
+
+    //Moderates the change in position based on sensitivity value
+    const float sensitivity = 0.025f;
+    xOffset *= sensitivity;
+    yOffset *= sensitivity;
+
+    //Adjusts yaw & pitch values against changes in positions
+    cameraYaw += xOffset;
+    cameraPitch += yOffset;
+
+    //Prevents turning up & down beyond 90 degrees to look backwards
+    if (cameraPitch > 89.0f)
+    {
+        cameraPitch = 89.0f;
+    }
+    else if (cameraPitch < -89.0f)
+    {
+        cameraPitch = -89.0f;
+    }
+
+    //Modification of direction vector based on mouse turning
+    vec3 direction;
+    direction.x = cos(radians(cameraYaw)) * cos(radians(cameraPitch));
+    direction.y = sin(radians(cameraPitch));
+    direction.z = sin(radians(cameraYaw)) * cos(radians(cameraPitch));
+    cameraFront = normalize(direction);
+} 
